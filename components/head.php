b4819e1eaa206939f1e7a5026a5e12ed54fbf99a
Ajay Mathur <ajaymathur@Ajays-MacBook-Pro.local>
2026-08-23 14:58:56 +0530

    Integrate Google Tag Manager (GTM-TBKXJRWX) container tracking

--- components/head.php
+++ components/head.php
@@ -31,6 +31,14 @@ $ogImageVal = !empty($ogImage) ? (str_starts_with($ogImage, 'http') ? $ogImage :
 <!DOCTYPE html>
 <html lang="en-IN">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TBKXJRWX');</script>
+    <!-- End Google Tag Manager -->
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -113,3 +121,7 @@ $ogImageVal = !empty($ogImage) ? (str_starts_with($ogImage, 'http') ? $ogImage :
     <?php endif; ?>
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBKXJRWX"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
